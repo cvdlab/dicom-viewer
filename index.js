5b32275eba4c3d7a5dd7517fb6154cf4087e516e
enricomarino <enrico.marino@email.com>
2017-07-03 16:58:53 +0200

    Update API

--- index.js
+++ index.js
@@ -5,14 +5,20 @@ const app = express()
 
 const APP_PORT = 3000
 const API_VERSION = '0'
-const API_ROOT = path.join('api', API_VERSION)
+const API_ROOT = path.join('/api', API_VERSION)
 
 app.use(express.static('public'))
 
-app.get(path.join(API_ROOT, 'slices/:coord/:number'), function (req, res) {
+/**
+ * GET /api/0/dicom/:name/:coord/:slice
+ */
+
+app.get(path.join(API_ROOT, 'dicom/:name/:coord/:slice'), function (req, res) {
+  let name = req.params.name
   let coord = req.params.coord
-  let number = req.params.number
-  res.send(`slice ${coord} ${number}`)
+  let slice = req.params.slice
+
+  res.send(`slice ${name} ${coord} ${slice}`)
 })
 
 app.listen(APP_PORT, function () {
